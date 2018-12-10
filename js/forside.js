c64152ff31a15bd26d73b699831e72120785adfc
Erik Granberg <36399140+erik8036granberg@users.noreply.github.com>
2018-12-10 10:03:06 +0100

    Indhenter valgte produkter til forside

--- js/forside.js
+++ js/forside.js
@@ -1,5 +1,19 @@
 //Hent mainpage indhold
 
+//	global var
+let produkter = [];
+let produkt;
+let klon;
+
+let produktTemplate = document.querySelector("[data-produktTemplate]");
+let produktContainer = document.querySelector("[data-produktContainer]");
+
+//vars til valg af produkter
+
+let valg_1;
+let valg_2;
+let valg_3;
+
 //		dokument DOM loadet
 document.addEventListener("DOMContentLoaded", hentJson);
 
@@ -18,6 +32,16 @@ async function hentJson() {
 	//	test json-import
 	console.log(side);
 
+	//	Indhent valgt produkter på forsiden
+	valg_1 = side.acf.vaelg_produkt_1;
+	valg_2 = side.acf.vaelg_produkt_2;
+	valg_3 = side.acf.vaelg_produkt_3;
+
+	//	test valg
+	console.log(valg_1);
+	console.log(valg_2);
+	console.log(valg_3);
+
 	//	Klon & indsæt i DOM
 	let klon = pageTemplate.cloneNode(true).content;
 	klon.querySelector("[data-overskrift]").textContent = side.acf.overskrift;
@@ -28,11 +52,6 @@ async function hentJson() {
 //------------------------------------------------------------
 
 //Hent produkter
-
-//	global var
-let produkter = [];
-let produktTemplate = document.querySelector("[data-produktTemplate]");
-let produktContainer = document.querySelector("[data-produktContainer]");
 
 //	dokument DOM loadet
 document.addEventListener("DOMContentLoaded", hentJsonProdukter);
@@ -49,36 +68,73 @@ async function hentJsonProdukter() {
 	//	test json-import
 	console.log(produkter);
 
-	//	begræns antal post vist på siden
-	antalProdukter = produkter.slice(0, 3);
-	console.log(antalProdukter);
-
 	visProdukter();
 }
 
-//	Event-loop
+
+//	Indhent produkter
 function visProdukter() {
 
-	//	Kør loop med json-data
-	antalProdukter.forEach(produkt => {
+	//	Kør loop for produkt 1
+	produkter.forEach(produkt => {
 		console.log(produkt);
 
-		//		Klon? ja tak
-		let klon = produktTemplate.cloneNode(true).content;
+		//	Filtrer produkter til de 3 valgte på forsiden
+		if (produkt.id == valg_1) {
 
-		klon.querySelector("[data-billede]").setAttribute("src", produkt.acf.photo.sizes.medium_large);
-		klon.querySelector("[data-billede]").setAttribute("alt", "Eventbillede for: " + produkt.title.rendered);
-		klon.querySelector("[data-title]").textContent = produkt.acf.title;
-		klon.querySelector("[data-description]").textContent = produkt.acf.short_description;
+			//		Klon? ja tak
+			let klon = produktTemplate.cloneNode(true).content;
 
+			klon.querySelector("[data-billede]").setAttribute("src", produkt.acf.photo.sizes.medium_large);
+			klon.querySelector("[data-billede]").setAttribute("alt", produkt.acf.short_description);
+			klon.querySelector("[data-title]").textContent = produkt.acf.title;
+			klon.querySelector("[data-description]").textContent = produkt.acf.short_description;
 
-		//			klon.querySelector(".event-wrapper").addEventListener("click", () => {
-		//				window.location.href = "produkter.html?id=" + event.id;
-		//				//				window.location.href = "produkter.html?id=" + event.id + "&preload=" + ("src", event.acf.billede);
-		//			});
+			//	    tilføj html DOM
+			produktContainer.appendChild(klon);
+			console.log("produkt er indlæst");
+		}
+	});
 
-		//	    tilføj html DOM
-		produktContainer.appendChild(klon);
-		console.log("loop er kørt");
+	//	Kør loop for produkt 2
+	produkter.forEach(produkt => {
+		console.log(produkt);
+
+		//	Filtrer produkter til de 3 valgte på forsiden
+		if (produkt.id == valg_2) {
+
+			//		Klon? ja tak
+			let klon = produktTemplate.cloneNode(true).content;
+
+			klon.querySelector("[data-billede]").setAttribute("src", produkt.acf.photo.sizes.medium_large);
+			klon.querySelector("[data-billede]").setAttribute("alt", produkt.acf.short_description);
+			klon.querySelector("[data-title]").textContent = produkt.acf.title;
+			klon.querySelector("[data-description]").textContent = produkt.acf.short_description;
+
+			//	    tilføj html DOM
+			produktContainer.appendChild(klon);
+			console.log("produkt er indlæst");
+		}
+	});
+
+	//	Kør loop for produkt 3
+	produkter.forEach(produkt => {
+		console.log(produkt);
+
+		//	Filtrer produkter til de 3 valgte på forsiden
+		if (produkt.id == valg_3) {
+
+			//		Klon? ja tak
+			let klon = produktTemplate.cloneNode(true).content;
+
+			klon.querySelector("[data-billede]").setAttribute("src", produkt.acf.photo.sizes.medium_large);
+			klon.querySelector("[data-billede]").setAttribute("alt", produkt.acf.short_description);
+			klon.querySelector("[data-title]").textContent = produkt.acf.title;
+			klon.querySelector("[data-description]").textContent = produkt.acf.short_description;
+
+			//	    tilføj html DOM
+			produktContainer.appendChild(klon);
+			console.log("produkt er indlæst");
+		}
 	});
 }
