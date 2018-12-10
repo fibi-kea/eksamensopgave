bc8b8bdd0fe6cf1270c0414242f2b8cfc11b9324
Fríðbjartur Miðberg <fm@Fribjarturs-MacBook-Pro.local>
2018-12-10 21:30:16 +0100

    > Fin justeringer af design
    >Nyheds site
    > Kontakt
    > About
    > Accordion

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+/*Collapsible on the About page*/
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var collapse_content = this.nextElementSibling;
+        if (collapse_content.style.maxHeight) {
+            collapse_content.style.maxHeight = null;
+        } else {
+            collapse_content.style.maxHeight = collapse_content.scrollHeight + "px";
+        }
+    });
+}
+/*Collapse end*/
