086dbafba8d91d73002b976a903772de10fde004
Erik Granberg <36399140+erik8036granberg@users.noreply.github.com>
2018-12-10 13:28:31 +0100

    Dataindhentning lagt på html-tag

--- js/forside.js
+++ js/forside.js
@@ -14,33 +14,30 @@ let valg_3;
 document.addEventListener("DOMContentLoaded", hentJson);
 
 
-
-//--------------------------- Indhent sideindhold til splash & produktvalg ---------------------------------
-
+//--------------------------- Indhent sideindhold ---------------------------------
 
 
 //	hent json
 async function hentJson() {
 	console.log("hentJson");
 
-	let pageTemplate = document.querySelector("[data-template]");
-	let pageContainer = document.querySelector("[data-container]");
-
 	//	Hent json-data wordpress content
 	let jsonData = await fetch("http://imkennykennedy.com/kea/2_semester/tema_eksamen/wordpress/wp-json/wp/v2/pages/542");
 
-	side = await jsonData.json();
+	let side = await jsonData.json();
 
 	//	test json-import
 	console.log(side);
 
-	//	Klon & indsæt i DOM
-	let klon = pageTemplate.cloneNode(true).content;
-	klon.querySelector("[data-overskrift]").textContent = side.acf.overskrift;
-	klon.querySelector("[data-underrubrik]").textContent = side.acf.underrubrik;
-	pageContainer.appendChild(klon);
+	let dest = document.querySelector("[data-container]");
 
-	//	Indhent valgt produkter på forsiden
+	//	indsæt i DOM
+
+	//	Splash
+	dest.querySelector("[data-overskrift]").textContent = side.acf.overskrift;
+	dest.querySelector("[data-underrubrik]").textContent = side.acf.underrubrik;
+
+	//	Produkter - indhent valg
 	valg_1 = side.acf.vaelg_produkt_1;
 	valg_2 = side.acf.vaelg_produkt_2;
 	valg_3 = side.acf.vaelg_produkt_3;
@@ -49,17 +46,18 @@ async function hentJson() {
 	console.log(valg_1);
 	console.log(valg_2);
 	console.log(valg_3);
+
+	//	Indlæs produkter
+	hentJsonProdukter();
+
+	//	about
 }
 
 
 
-//--------------------------- Indhent produkter ---------------------------------
+//--------------------------- Indlæs produkter ---------------------------------
 
 
-
-
-//	dokument DOM loadet
-document.addEventListener("DOMContentLoaded", hentJsonProdukter);
 
 //	hent json
 async function hentJsonProdukter() {
@@ -158,7 +156,3 @@ function visProdukter() {
 		}
 	});
 }
-
-
-
-//------------------------------------------------------------
