e292b1e049e1b06d772ae6a302c0f7c33cc9fec9
Fríðbjartur Miðberg <fm@ejer.kea.local>
2018-12-11 10:31:49 +0100

    Rest API javascript

---
+++ js/burger.js
@@ -0,0 +1,45 @@
+if (window.innerWidth < 991) {
+
+
+
+
+    //		nav-menu-mobile - eventlisterns
+
+    document.addEventListener("DOMContentLoaded", function (event) {
+        navMenu();
+    });
+
+    //	viser & skjuler navigationsmenu
+
+    function navMenu() {
+
+        console.log("navMenu");
+
+        function toggleMenu() {
+            console.log("toggleMenu");
+            document.querySelector(".burger").classList.toggle("change");
+            document.querySelector("nav").classList.toggle("show");
+            document.querySelector("header").classList.toggle("show");
+        }
+
+        function closeMenu() {
+            console.log("closeMenu");
+            document.querySelector(".burger").classList.remove("change");
+            document.querySelector("nav").classList.remove("show");
+            document.querySelector("header").classList.remove("show");
+        }
+
+        document.querySelector(".burger").addEventListener("click", toggleMenu);
+        document.querySelector("nav ul").addEventListener("click", closeMenu);
+
+        //	luk menu, hvis der bruges enter-knap i searchfeltet
+        let input = document.querySelector("#search");
+        input.addEventListener("keyup", function (event) {
+            event.preventDefault();
+            if (event.keyCode === 13) {
+                closeMenu();
+            }
+        });
+
+    }
+}
